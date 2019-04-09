e8fd2259d37a36cfe04f847944a06ae5c133ad46
Steve Thompson <writetome51@gmail.com>
2019-04-09 13:00:38 -0600

    new commit

--- tests.js
+++ tests.js
@@ -1,22 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var isString_notString_1 = require("./index");
+var isString = require("./index").isString;
+var notString = require("./index").notString;
+
+
 var strings = ['', ' ', '	', '\n', '\r', '\\', '\'', '"', '`', '1', '0', '!@#$%^&*()'];
 var nonStrings = [true, 1, 0.0001, [8, 9], {}, function (i) { return (i === 2); }, false, { prop: 1 }, ['hello']];
+
 // Test 1:  This must return true each time to pass:
 var results = [];
 for (var i = 0; i < strings.length; ++i) {
-    results.push(isString_notString_1.isString(strings[i]));
+    results.push(isString(strings[i]));
 }
 if (results.length === strings.length && !(results.includes(undefined))
     && !(results.includes(false)) && results.includes(true))
     console.log('test 1 passed');
 else
     console.log('test 1 FAILED');
+
+
 // Test 2:  This must return false each time to pass:
 results = [];
 for (var i = 0; i < nonStrings.length; ++i) {
-    results.push(isString_notString_1.isString(nonStrings[i]));
+    results.push(isString(nonStrings[i]));
 }
 if (results.length === nonStrings.length && !(results.includes(undefined))
     && !(results.includes(true)) && results.includes(false))
@@ -26,7 +32,7 @@ else
 // Test 3:  This must return true each time to pass:
 results = [];
 for (var i = 0; i < nonStrings.length; ++i) {
-    results.push(isString_notString_1.notString(nonStrings[i]));
+    results.push(notString(nonStrings[i]));
 }
 if (results.length === nonStrings.length && !(results.includes(undefined))
     && !(results.includes(false)) && results.includes(true))
@@ -36,7 +42,7 @@ else
 // Test 4:  This must return false each time to pass:
 results = [];
 for (var i = 0; i < strings.length; ++i) {
-    results.push(isString_notString_1.notString(strings[i]));
+    results.push(notString(strings[i]));
 }
 if (results.length === strings.length && !(results.includes(undefined))
     && !(results.includes(true)) && results.includes(false))
